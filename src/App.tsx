6dc8af5af7b97d25c33b55ed43d16d9941989408
Aldi-H <aldihimawan88@gmail.com>
2024-09-16 16:48:14 +0700

    Refactor App component to include Header, Modal, and NoteItem components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Header from "./components/Header";
+import Modal from "./components/Modal";
+import NoteItem from "./components/NoteItem";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleToggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="max-w-screen-xl m-auto">
+      <Modal isOpen={isModalOpen} onClose={handleToggleModal} />
+      <Header onOpenModal={handleToggleModal} />
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 grid-flow-row gap-4 ">
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
